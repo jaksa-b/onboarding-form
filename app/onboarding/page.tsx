7048c8e1278f35f7188ef38a63be9a67da4c83ce
Jakša Bašić <jaksabasic1@gmail.com>
2024-10-09 17:44:41 +0200

    setup: react testing library

---
+++ app/onboarding/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Onboarding = () => {
+  return (
+    <form>
+      <label htmlFor="first-name">First name</label>
+      <input id="first-name" />
+    </form>
+  );
+};
+
+export default Onboarding;
